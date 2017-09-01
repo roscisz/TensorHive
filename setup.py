2d031e98bb3ce97558e9aecb5b56839a65474c24
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2017-09-01 17:18:05 +0200

    add package data in setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'tensorhive',
-    version = '0.1',
+    version = '0.1.1',
     packages = find_packages(),
-    entry_points={
+    package_data = {'tensorhive': ['scripts/*', 'static/*']},
+    entry_points = {
         'console_scripts': [
             'tensorhive = tensorhive.__main__:main'
         ],
@@ -13,8 +14,9 @@ setup(
     author = 'Pawel Rosciszewski',
     author_email = 'pawel.rosciszewski@pg.edu.pl',
     url = 'https://github.com/roscisz/TensorHive',
-    download_url = 'https://github.com/roscisz/TensorHive/archive/0.1.tar.gz',
+    download_url = 'https://github.com/roscisz/TensorHive/archive/0.1.1.tar.gz',
     keywords = 'distributed machine learning tensorflow resource management',
     install_requires=['kernelhive>=1.2.7', 'pyrrd'],
+    include_package_data=True,
     zip_safe=False
 )
