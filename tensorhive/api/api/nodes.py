e9db350fa982dd89ccbb4776c67e2f46c14caef9
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-03 20:43:54 +0200

    First gpu parameter endpoint - fan.speed (needs refactor)

--- tensorhive/api/api/nodes.py
+++ tensorhive/api/api/nodes.py
@@ -12,4 +12,8 @@ def get_all_metrics():
 
 
 def get_gpu_metrics(hostname):
-    return GetMetricsController.get(hostname=hostname, metric_type='GPU')
+    return GetMetricsController.gpu(hostname=hostname, metric_type='GPU')
+
+
+def get_gpu_fan(hostname):
+    return GetMetricsController.get(hostname=hostname, metric_type='GPU', parameter_name='fan.speed [%]')
