40b35c5b7cfb70fab8d9eca5734198e18032be87
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-03 20:43:54 +0200

    First gpu parameter endpoint - fan.speed (needs refactor)

--- tensorhive/controllers/node/GetMetricsController.py
+++ tensorhive/controllers/node/GetMetricsController.py
@@ -9,10 +9,13 @@ class GetMetricsController:
         return infrastructure, 200
 
     @staticmethod
-    def get(hostname: str, metric_type: str):
+    def get(hostname: str, metric_type: str, parameter_name: str = None):
         try:
             infrastructure = TensorHiveManager().infrastructure_manager.infrastructure
             metrics = infrastructure[hostname][metric_type]
+
+            if parameter_name is not None:
+                metrics = [resource[parameter_name] for resource in metrics]
 
             # If some data is unavailable, return [] (e.g. monitoring service got a connection exception),
             if isinstance(metrics, dict) and metrics.keys() & ['exception', 'exit_code']:
@@ -22,3 +25,4 @@ class GetMetricsController:
             response = NoContent, 404
         finally:
             return response
+    
