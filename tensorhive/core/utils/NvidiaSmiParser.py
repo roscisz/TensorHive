a7839e47b8ede8086dcf107acf328230b98147c4
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-07 14:04:25 +0200

    Use shorter nvidia-smi key names (spec+impl)

--- tensorhive/core/utils/NvidiaSmiParser.py
+++ tensorhive/core/utils/NvidiaSmiParser.py
@@ -1,7 +1,25 @@
 from typing import Generator, Dict, List
+import logging
+log = logging.getLogger(__name__)
 
 
 class NvidiaSmiParser():
+
+    key_mapping = {
+        # keys: original nvidia-smi parameter names
+        # values: simpler and shorter form
+        'name': 'name',
+        'uuid': 'uuid',
+        'fan.speed [%]': 'fan_speed',
+        'memory.free [MiB]': 'mem_free',
+        'memory.used [MiB]': 'mem_used',
+        'memory.total [MiB]': 'mem_total',
+        'utilization.gpu [%]': 'gpu_util',
+        'utilization.memory [%]': 'mem_util',
+        'temperature.gpu': 'temp',
+        'power.draw [W]': 'power'
+    }
+
     @classmethod
     def _format_values(cls, values: List[str]):
         def formatted_value(value):
@@ -17,6 +35,19 @@ class NvidiaSmiParser():
         return [formatted_value(v) for v in values]
 
     @classmethod
+    def _renamed_keys(cls, original_keys: List[str]):
+        '''Replaces nvidia-smi query key names with more compact alternatives'''
+        try:
+            new_keys = [cls.key_mapping[original_key]
+                        for original_key in original_keys]
+            return new_keys
+        except KeyError as unexisting_key:
+            message = 'key mapping for {} not implemented!'.format(
+                unexisting_key)
+            log.critical(message)
+            raise KeyError(message)
+
+    @classmethod
     def gpus_info_from_stdout(cls, stdout: Generator) -> Dict[str, str]:
         '''
         Assumes nvidia-smi outputs with --format=csv (+must have a header)
@@ -25,17 +56,16 @@ class NvidiaSmiParser():
             {
                 "name": "GeForce GTX 660",
                 "uuid": "GPU-d38d4de3-85ee-e837-3d87-e8e2faeb6a63",
-                "fan.speed [%]": "33",
-                "memory.free [MiB]": "967",
-                "memory.used [MiB]": "1029",
-                "memory.total [MiB]": "1996",
-                "utilization.gpu [%]": null,
-                "utilization.memory [%]": null,
-                "temperature.gpu": "46",
-                "power.draw [W]": null
+                "fan_speed": 32,
+                "mem_free": 1329,
+                "mem_used": 667,
+                "mem_total": 1996,
+                "gpu_util": null,
+                "mem_util": null,
+                "temp": 44,
+                "power": null
             }
         ]
-
         '''
         stdout_lines = list(stdout)  # type: List[str]
         assert stdout_lines, 'stdout is empty!'
@@ -44,11 +74,12 @@ class NvidiaSmiParser():
         # Extract keys from nvidia-smi query result header
         header = stdout_lines[0]  # type: str
         gpu_parameters_keys = header.split(', ')  # type: List[str]
+        gpu_parameters_keys = cls._renamed_keys(gpu_parameters_keys)
 
         # Extract stdout lines, where:  1 line = 1 GPU
         all_gpus_stdout_lines = stdout_lines[1:]  # type: List[str]
 
-        # Result accumulator
+        # Define result accumulator
         all_gpus_info = []  # type:List[Dict[str, str]]
 
         # Transform stdout of a single GPU and append to accumulator
