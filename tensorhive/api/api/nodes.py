ae7125b40e5a472a5faca232737cb3aa1938b676
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-07 14:04:25 +0200

    Use shorter nvidia-smi key names (spec+impl)

--- tensorhive/api/api/nodes.py
+++ tensorhive/api/api/nodes.py
@@ -12,20 +12,6 @@ def get_all_metrics():
 
 
 def get_gpu_metrics(hostname: str, metric_type: str = None):
-    query_to_key_mapping = {
-        'name': 'name',
-        'uuid': 'uuid',
-        'fan': 'fan.speed [%]',
-        'mem_free': 'memory.free [MiB]',
-        'mem_used': 'memory.used [MiB]',
-        'mem_total': 'memory.total [MiB]',
-        'util': 'utilization.gpu [%]',
-        'mem_util': 'utilization.memory [%]',
-        'temp': 'temperature.gpu',
-        'power': 'power.draw [W]'
-    }
-    if metric_type is not None:
-        metric_type = query_to_key_mapping[metric_type]
     return GetMetricsController.get(hostname=hostname,
                                     resource_type='GPU',
                                     metric_type=metric_type)
