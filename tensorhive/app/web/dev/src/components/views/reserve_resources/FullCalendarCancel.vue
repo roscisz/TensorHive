82a94ec320c4cca4630cbfd5565aebf3a5168000
Filip Schodowski <filipschodowski@gmail.com>
2018-08-09 21:33:26 +0200

    Fix reservation time selection

---
+++ tensorhive/app/web/dev/src/components/views/reserve_resources/FullCalendarCancel.vue
@@ -0,0 +1,58 @@
+<template>
+  <BaseModal
+    :show="showModal"
+    @close="close"
+  >
+    <div class="modal-header">
+      <h3>Do you want to cancel this reservation?</h3>
+    </div>
+    <div class="modal-body">
+    </div>
+    <div class="modal-footer text-right">
+      <button
+        class="modal-default-button"
+        @click="cancelReservation()"
+      >
+        Cancel
+      </button>
+    </div>
+  </BaseModal>
+</template>
+
+<script>
+import BaseModal from './BaseModal.vue'
+export default {
+  name: 'FullCalendarCancel',
+
+  props: {
+    showModal: Boolean,
+    reservation: Object,
+    cancel: Function
+
+  },
+
+  components: {
+    BaseModal
+  },
+
+  data () {
+    return {
+      title: '',
+      body: ''
+    }
+  },
+
+  methods: {
+    close: function () {
+      this.$emit('close')
+      this.title = ''
+      this.body = ''
+    },
+
+    cancelReservation: function () {
+      this.cancel(this.reservation)
+      this.close()
+    }
+  }
+}
+</script>
