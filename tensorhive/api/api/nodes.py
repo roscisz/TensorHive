58af090755cd073311746d456d126e65a5352459
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-19 16:36:47 +0200

    Redesign endpoints (info, metrics, processes).

--- tensorhive/api/api/nodes.py
+++ tensorhive/api/api/nodes.py
@@ -1,4 +1,4 @@
-from tensorhive.controllers.node.GetMetricsController import GetMetricsController
+from tensorhive.controllers.node.GetGPUController import GetGPUController
 from tensorhive.controllers.node.GetHostnamesController import GetHostnamesController
 from connexion import NoContent
 
@@ -8,10 +8,15 @@ def get_hostnames():
 
 
 def get_all_metrics():
-    return GetMetricsController.all()
+    return GetGPUController.all()
 
 
 def get_gpu_metrics(hostname: str, metric_type: str = None):
-    return GetMetricsController.get(hostname=hostname,
-                                    resource_type='GPU',
+    return GetGPUController.get_metrics(hostname=hostname,
                                     metric_type=metric_type)
+
+def get_gpu_processes(hostname: str):
+    return GetGPUController.get_processes(hostname=hostname)
+
+def get_gpu_info(hostname: str):
+    return GetGPUController.get_info(hostname=hostname)
