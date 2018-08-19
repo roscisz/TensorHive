56ddaf7dea5e3f64c6eacf03be03738a94cad5bd
Michał Martyniak <michal.martyniak@linux.pl>
2018-08-19 14:23:21 +0200

    Refactor GPUMonitoringBehaviour. Fix bash script bug.
    nvidia-smi pmon command would exit after first failure, but what if there would more GPUs which supports pmon - found 'n fix

--- tensorhive/core/monitors/GPUMonitoringBehaviour.py
+++ tensorhive/core/monitors/GPUMonitoringBehaviour.py
@@ -6,28 +6,25 @@ from pssh.exceptions import Timeout, UnknownHostException, ConnectionErrorExcept
 import logging
 log = logging.getLogger(__name__)
 
+
 class GPUMonitoringBehaviour(MonitoringBehaviour):
-    
+    '''Responsible for fetching data about installed GPUs within configured network'''
 
     @override
     def update(self, group_connection) -> Dict:
         metrics = self._query_gpu_for_metrics(group_connection)  # type: Dict
         processes = self._current_processes(group_connection)  # type: Dict
         result = self._combine_outputs(metrics, processes)  # type: Dict
-        #import json
-        #log.debug('METRYKI\n{}\n'.format(json.dumps(metrics, indent=4)))
-        #log.debug('PROCESY\n{}\n'.format(json.dumps(processes, indent=4)))
-        #dict_merge(metrics, processes)
-        #metrics = self._query_gpu_for_metrics(group_connection)  # type: Dict
-        #processes = self._current_processes(group_connection)  # type: Dict
-        #dict_merge(processes, metrics)
-
-        #log.debug('MERGE\n{}\n'.format(json.dumps(result, indent=4)))
-        #result = metrics
         return result
 
     @property
     def composed_query_command(self) -> str:
+        '''
+        Builds a query command for nvidia-smi that can be executed on each node
+
+        Example result:
+        nvidia-smi --query-gpu=temperature.gpu,utilization.gpu,utilization.memory --format=csv
+        '''
         base_command = 'nvidia-smi --query-gpu='
         format_options = '--format=csv,nounits'
         available_queries = [
@@ -44,7 +41,6 @@ class GPUMonitoringBehaviour(MonitoringBehaviour):
             'power.draw'
         ]
 
-        # Example: nvidia-smi --query-gpu=temperature.gpu,utilization.gpu,utilization.memory --format=csv
         query = ','.join(available_queries)
         command = '{base_command}{query} {format_options}'.format(
             base_command=base_command,
@@ -52,34 +48,11 @@ class GPUMonitoringBehaviour(MonitoringBehaviour):
             format_options=format_options)
         return command
 
-    @property
-    def get_gpu_processes_command(self):
-        return '''
-            UUIDS=$(nvidia-smi --query-gpu=uuid --format=csv,noheader)
-
-            if [ $? -eq 0 ]; then
-                echo $UUIDS | while read line; do
-                    PROCESSES=$(nvidia-smi pmon --count 1 --id "$line")
-                    
-                    if [ $? -eq 0 ]; then
-                        echo "UUID=$line"
-                        echo "$PROCESSES"
-                    else
-                        # nvidia-smi pmon is not supported
-                        exit $?
-                    fi
-                done 
-            else
-                # nvidia-smi failed
-                exit $?
-            fi
-        '''
-
     def _query_gpu_for_metrics(self, group_connection) -> Dict:
         '''
-        Executes a query on each node within group_connection, then it 
-        returns gathered information as a dictionary.
-    
+        Executes a query on each node within group_connection, then
+        it returns gathered information as a dictionary.
+
         Example result:
         {
             'example_host_0': {
@@ -118,30 +91,77 @@ class GPUMonitoringBehaviour(MonitoringBehaviour):
                     log.error('nvidia-smi raised {} on {}'.format(host_out.exception.__class__.__name__, host))
                 metrics = None
             result[host] = {'GPU': metrics}
-        #import json
-        #log.debug('\n{}\n'.format(json.dumps(result, indent=2)))
         return result
 
-    def _get_process_owner(self, pid: int, hostname: str, group_connection) -> str:
-        '''Use single-host connection to acquire process owner'''
-        # TODO Move common to SSHConnectionManager
-        connection = group_connection.host_clients[hostname]
+    def _get_process_owner(self, pid: int, hostname: str, connection) -> str:
+        '''Use single-host connection to acquire process owner using `ps`'''
         command = 'ps --no-headers -o user {}'.format(pid)
+        connection = connection.host_clients[hostname]  # type: SSHClient
 
         output = connection.run_command(command)
-        channel, hostname, stdout, stderr, _ = output
+        _, hostname, stdout, stderr, _ = output
 
         result = list(stdout)
         if not result:
             # Empty output -> Process with such pid does not exist
             return None
-        # Extract owner from list ['example_owner']
+        # Unpack owner from the list (e.g. ['example_owner'])
         return result[0]
+
+    @property
+    def get_gpu_processes_command(self):
+        '''
+        Returns short bash script that can be executed on each node.
+        Script tries to execute nvidia-smi pmon for each UUID separately.
+
+        Explanation for this is that GPUs' indexes are not fixed in time,
+        hence UUID parameter.
+
+        When executed, gives output, like:
+            UUID=GPU-c6d01ed6-8240-2e11-efe9-1111111111111
+            # gpu        pid  type    sm   mem   enc   dec   command
+            # Idx          #   C/G     %     %     %     %   name
+                0       1979     G     0     3     0     0   X
+                1       1234     G     0    90     0     0   python
+                1       4567     G     0    89     0     0   python
+            UUID=GPU-c6d01ed6-8240-2e11-efe9-2222222222222
+            # gpu        pid  type    sm   mem   enc   dec   command
+            # Idx          #   C/G     %     %     %     %   name
+                0       1979     G     0     3     0     0   X
+                1       1234     G     0    90     0     0   python
+                1       4567     G     0    89     0     0   python
+            UUID=GPU-7fcc76c8-ac23-0ead-83ce-3f6f3d831d8a
+            [PMON NOT SUPPORTED]
+        '''
+        return '''
+            # Get a list of UUIDs of each installed GPU in the system
+            UUIDS=$(nvidia-smi --query-gpu=uuid --format=csv,noheader)
+
+            # Check exit code
+            if [ $? -eq 0 ]; then
+                # Success (nvidia-smi is installed)
+                # Read UUIDs, 1 line = 1 UUID
+                echo "$UUIDS" | while read line; do
+                    echo "UUID=$line"
+
+                    # Fetch a list of processes on this GPU
+                    PROCESSES=$(nvidia-smi pmon --count 1 --id "$line")
+
+                    if [ $? -eq 0 ]; then
+                        echo "$PROCESSES"
+                    else
+                        echo "[PMON NOT SUPPORTED]"
+                    fi
+                done 
+            else
+                # nvidia-smi failed
+                exit $?
+            fi
+        '''
 
     def _current_processes(self, group_connection) -> Dict:
         '''
         Fetches the information about all active gpu processes using nvidia-smi pmon
-        On each node, `pmon` is called for each GPU (with --id <UUID>) in order to keep consistency
 
         Example result:
         {
@@ -162,63 +182,66 @@ class GPUMonitoringBehaviour(MonitoringBehaviour):
         result = {}
         for host, host_out in output.items():
             if host_out.exit_code is 0:
-                processes = NvidiaSmiParser.parse_pmon_stdout(
-                    host_out.stdout)
-                # Find each process owner
+                processes = NvidiaSmiParser.parse_pmon_stdout(host_out.stdout)
+                # Find process owner for each process
                 for process in processes:
                     process['owner'] = self._get_process_owner(process['pid'], host, group_connection)
             else:
                 # Possible reasons:
                 # - nvidia-smi not installed
-                # - nvidia-smi pmon is not supported
                 # - could not connect to host
                 processes = None
             result[host] = processes
-        # import json
-        # log.debug('\n{}\n'.format(json.dumps(result, indent=2)))
         return result
 
     def _combine_outputs(self, metrics: Dict, processes: Dict) -> Dict:
         '''
-        Merges dicts from 
-        > nvidia-smi --query
-        > nvidia-smi pmon
+        Merges two dicts in place
 
         Example result:
-        TODO
+        {
+            "des13.kask": {
+                "GPU": {
+                    "GPU-c6d01ed6-8240-2e11-efe9-aa32794b8273": {
+                        "name": "GeForce GTX 1060 6GB",
+                        "index": 0,
+                        "metrics": {
+                            "fan_speed": {
+                                "value": 25,
+                                "unit": "%"
+                            },
+                            "temp": 40,
+                            ...
+                        },
+                        "processes": [
+                            {
+                                "pid": 1979,
+                                "command": "X",
+                                "owner": "root"
+                            }
+                        ]
+                    }
+                }
+            },
+        }
         '''
-        # import json
-        # log.debug('\n{}\n'.format(json.dumps(metrics, indent=4)))
-        # log.debug('\n{}\n'.format(json.dumps(processes, indent=4)))
-        # TODO May want to refactor in the future
         for hostname, gpu_processes_on_node in processes.items():
-            # Loop thorugh each item on GPU list and create a new key with default value
-            # node_gpus = metrics[hostname]['GPU']  # type: List[Dict]
-            # for uuid, data in node_gpus.items():
-            #     metrics[hostname]['GPU'][uuid]['processes'] = []
-
-            # Loop through all processes and assign them into corresponding places in a list
-            # type: List[Dict]
-            
-
-
-            # if gpu_processes_on_node is None:
-            #     for uuid, data in metrics[hostname]['GPU'].items():
-            #         metrics[hostname]['GPU'][uuid]['processes'] = None
-            
-            if metrics[hostname]['GPU'] is None:
-                # Example reason: counld not connect to host, hence no data
+            if metrics[hostname].get('GPU') is None:
+                # Can't access any GPU right now, e.g. could not connect to host or nvidia-smi failure
                 continue
 
-            # FIXME Need to refactor
             if gpu_processes_on_node is None:
+                # _current_processes could not fetch any info about processes
+                # Create 'processes' key for each GPU and fill with None
                 for uuid, _ in metrics[hostname]['GPU'].items():
                     metrics[hostname]['GPU'][uuid]['processes'] = None
             else:
+                # Prepare 'processes' key so that we can append processes later
                 for uuid, _ in metrics[hostname]['GPU'].items():
                     metrics[hostname]['GPU'][uuid]['processes'] = []
 
+                # Unpack every known process and move to the corresponding GPU
                 for process in gpu_processes_on_node:
-                    uuid = process.pop('uuid') 
+                    uuid = process.pop('uuid')
                     metrics[hostname]['GPU'][uuid]['processes'].append(process)
         return metrics
