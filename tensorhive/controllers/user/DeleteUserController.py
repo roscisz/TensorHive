9fb610ba49c3c6c9708d1d2f3780a41510c117f2
Tomasz Menet <tommen@onet.eu>
2018-10-05 21:28:54 +0200

    Blacklisting tokens in user destroy

--- tensorhive/controllers/user/DeleteUserController.py
+++ tensorhive/controllers/user/DeleteUserController.py
@@ -1,10 +1,10 @@
 from tensorhive.models.user.UserModel import UserModel
 from tensorhive.models.role.RoleModel import RoleModel
 from connexion import NoContent
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import get_raw_jwt
+from tensorhive.models.auth.RevokedTokenModel import RevokedTokenModel
 
 class DeleteUserController():
-    # TODO Add more user parameters
 
     @staticmethod
     def delete(id):
@@ -20,6 +20,17 @@ class DeleteUserController():
         if user is not None:
             if not user.delete_from_db():
                 return NoContent, 500
+            else:
+                jti = get_raw_jwt()['jti']
+                try:
+                    revoked_token = RevokedTokenModel(jti=jti)
+                except:
+                    return {
+                               'msg': 'Token has not been revoked due to error'
+                           }, 501
+
+                if not revoked_token.save_to_db():
+                    return NoContent, 500
         else:
             return NoContent, 404
         return NoContent, 204
