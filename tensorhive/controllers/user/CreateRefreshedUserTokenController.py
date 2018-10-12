bf8fc45e486d351f607b10b13ebd38426c2cfad5
Michał Martyniak <michal.martyniak@linux.pl>
2018-10-12 17:04:22 +0200

    Refactor token refreshing controller with yml repsonses

--- tensorhive/controllers/user/CreateRefreshedUserTokenController.py
+++ tensorhive/controllers/user/CreateRefreshedUserTokenController.py
@@ -1,8 +1,11 @@
 from flask_jwt_extended import create_access_token, get_jwt_identity
 from tensorhive.models.User import User
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.orm.exc import NoResultFound
+from tensorhive.config import API
 import logging
 log = logging.getLogger(__name__)
+R = API.RESPONSES
+G = API.RESPONSES['general']
 
 
 class CreateRefreshedUserTokenController():
@@ -13,18 +16,18 @@ class CreateRefreshedUserTokenController():
             current_user = User.get(id=get_jwt_identity())
             new_access_token = create_access_token(identity=current_user.id, fresh=False)
         except NoResultFound as e:
-            content, status = 'User does not exist in database', 404
-            log.error(e)
-        except MultipleResultsFound as e:
-            content, status = 'Internal error', 500
-            log.error(e)
+            log.warning(e)
+            content = {'msg': R['user']['not_found']}
+            status = 404
+        except Exception as e:
+            log.critical(e)
+            content = {'msg': G['internal_error']}
+            status = 500
         else:
             content = {
-                'msg': 'Refreshed token',
+                'msg': R['token']['refreshed'],
                 'access_token': new_access_token
             }
             status = 201
         finally:
-            if isinstance(content, str):
-                content = {'msg': content}, status
             return content, status
