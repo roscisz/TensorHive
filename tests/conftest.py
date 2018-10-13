066b640edda87c3b8764c88558d2f52c5efd3c73
Michał Martyniak <michal.martyniak@linux.pl>
2018-10-13 16:39:50 +0200

    WIP: Adjust tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 import pytest
-from tensorhive.database import create_app, db
+from tensorhive.database import create_boilerplate_app, db
 from tensorhive.models.Reservation import Reservation
 
 from fixtures.models import new_reservation, new_user, new_admin, user_role, admin_role
