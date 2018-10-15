a715f2514839d7c4f923c0912b829e24a15aae18
Michał Martyniak <michal.martyniak@linux.pl>
2018-10-15 16:13:50 +0200

    Refactor `Reservation` model: parse and validate datetime. Remove unused imports

--- tensorhive/models/Reservation.py
+++ tensorhive/models/Reservation.py
@@ -1,14 +1,9 @@
-import datetime
-
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, CheckConstraint, and_, not_, or_
-from sqlalchemy.exc import SQLAlchemyError, IntegrityError
-from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
-from sqlalchemy.orm import validates
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, and_, not_, or_
 from tensorhive.database import db, flask_app
-from tensorhive.models.User import User
 from tensorhive.models.CRUDModel import CRUDModel
-from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
+from typing import Optional
+import datetime
 import logging
 log = logging.getLogger(__name__)
 
@@ -22,8 +17,8 @@ class Reservation(CRUDModel, db.Model):
     protected_resource_id = Column(String(60), nullable=False)
 
     # Stored as UTC time
-    starts_at = Column(DateTime, nullable=False)
-    ends_at = Column(DateTime, nullable=False)
+    _starts_at = Column(DateTime, nullable=False)
+    _ends_at = Column(DateTime, nullable=False)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
 
     __display_datetime_format = '%Y-%m-%dT%H:%M:%S'
@@ -33,7 +28,8 @@ class Reservation(CRUDModel, db.Model):
     def check_assertions(self):
         assert self.user_id, 'Reservation owner must be given!'
         assert self.protected_resource_id, 'Reservation must be related with a resource!'
-        assert self.starts_at and self.ends_at, 'Reservation time range is not specified!'
+        assert self.starts_at, 'Reservation start time is invalid!'
+        assert self.ends_at, 'Reservation end time is invalid!'
         assert self.duration >= self.__min_reservation_time, 'Reservation duration is too short!'
 
         assert 8 < len(self.title) < 60, 'Reservation title length has incorrect length!'
@@ -43,26 +39,54 @@ class Reservation(CRUDModel, db.Model):
         collision = self.would_interfere()
         assert not collision, 'Reservation would interfere with some other reservation!'
 
-
     @hybrid_property
     def duration(self):
         return self.ends_at - self.starts_at
 
-    def __repr__(self):
-        return '''
-<ReservationEvent id={0}, user={1}
-    title={2}
-    description={3}
-    protected_resource_id={4}
+    @classmethod
+    def parsed_input_datetime(cls, value: str) -> Optional[datetime.datetime]:
+        client_datetime_format = '%Y-%m-%dT%H:%M:%S.%fZ'
+        try:
+            result = datetime.datetime.strptime(value, client_datetime_format)
+        except ValueError:
+            log.error('Could not parse datetime (value={})'.format(value))
+            result = None
+        finally:
+            return result
 
-    starts_at={5}
-    ends_at={6}
-    created_at={7}'''.format(
-            self.id, self.user,
-            self.title, self.description,
-            self.protected_resource_id,
-            self.starts_at, self.ends_at,
-            self.created_at)
+    @classmethod
+    def parsed_output_datetime(cls, value: datetime.datetime) -> str:
+        display_datetime_format = '%Y-%m-%dT%H:%M:%S'
+        server_timezone = '+00:00'
+        return value.strftime(display_datetime_format) + server_timezone
+
+    @hybrid_property
+    def starts_at(self):
+        return self._starts_at
+
+    @starts_at.setter
+    def starts_at(self, value):
+        if isinstance(value, str):
+            self._starts_at = self.parsed_input_datetime(value)
+        elif isinstance(value, datetime.datetime):
+            self._starts_at = value
+        else:
+            self._starts_at = None
+            log.error('Unsupported type (starts_at={})'.format(value))
+
+    @hybrid_property
+    def ends_at(self):
+        return self._ends_at
+
+    @ends_at.setter
+    def ends_at(self, value):
+        if isinstance(value, str):
+            self._ends_at = Reservation.parsed_input_datetime(value)
+        elif isinstance(value, datetime.datetime):
+            self._ends_at = value
+        else:
+            self._ends_at = None
+            log.error('Unsupported type (ends_at={})'.format(value))
 
     @classmethod
     def current_events(cls):
@@ -72,8 +96,8 @@ class Reservation(CRUDModel, db.Model):
             return cls.query.filter(
                 and_(
                     # Events that has already started
-                    cls.starts_at <= current_time, 
-                    # Events before their end 
+                    cls.starts_at <= current_time,
+                    # Events before their end
                     current_time <= cls.ends_at)
                 ).all()
 
@@ -112,21 +136,32 @@ class Reservation(CRUDModel, db.Model):
     # #         return False
     # #     # Check if any row were affected by deletion (otherwise -> not found)
     # #     return True if num_rows_deleted > 0 else False
+    def __repr__(self):
+        return '''
+<ReservationEvent id={0}, user={1}
+    title={2}
+    description={3}
+    protected_resource_id={4}
+
+    starts_at={5}
+    ends_at={6}
+    created_at={7}'''.format(
+            self.id, self.user,
+            self.title, self.description,
+            self.protected_resource_id,
+            self.starts_at, self.ends_at,
+            self.created_at)
 
     @property
     def as_dict(self):
         '''Serializes model instance into dict (which is interpreted as json automatically)'''
-        return dict(id=self.id,
-                    title=self.title,
-                    description=self.description,
-                    resourceId=self.protected_resource_id,
-                    userId=self.user_id,
-                    start=self.starts_at.strftime(
-                        self.__display_datetime_format)
-                    + self.__server_timezone,
-                    end=self.ends_at.strftime(
-                        self.__display_datetime_format)
-                    + self.__server_timezone,
-                    createdAt=self.created_at.strftime(
-                        self.__display_datetime_format)
-                    )
+        return {
+            'id': self.id,
+            'title': self.title,
+            'description': self.description,
+            'resourceId': self.protected_resource_id,
+            'userId': self.user_id,
+            'start': self.parsed_output_datetime(self.starts_at),
+            'end': self.parsed_output_datetime(self.ends_at),
+            'createdAt': self.parsed_output_datetime(self.created_at)
+        }
