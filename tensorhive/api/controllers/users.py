9ef79eb5ef570cf0720a36226da9b35e478f8999
Michał Martyniak <michal.martyniak@linux.pl>
2018-10-15 12:04:47 +0200

    Clean up unused files

--- tensorhive/api/controllers/users.py
+++
@@ -1,8 +0,0 @@
-from tensorhive.authorization import admin_required
-from tensorhive.controllers.user.ListUsersController import ListUsersController
-
-
-@admin_required
-def search():
-    '''Get all users'''
-    return ListUsersController.get()
