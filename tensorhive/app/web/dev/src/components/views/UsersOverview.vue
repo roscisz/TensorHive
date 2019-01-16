6be074d1ad8e063ab1ce09b11cbb5a2d49e8f0ed
Filip Schodowski <filipschodowski@gmail.com>
2019-01-16 22:00:39 +0100

    Allow user modification in web app

--- tensorhive/app/web/dev/src/components/views/UsersOverview.vue
+++ tensorhive/app/web/dev/src/components/views/UsersOverview.vue
@@ -11,6 +11,49 @@
       <div class="text-xs-center pt-2">
         <v-btn color="primary" @click="createUser()">Create user</v-btn>
       </div>
+      <v-dialog v-model="dialog" max-width="500px">
+        <v-card>
+          <v-card-text>
+            Edit user
+          </v-card-text>
+          <v-card-text>
+            <v-card-text>
+              New username:
+            </v-card-text>
+            <v-textarea
+              outline
+              label="Username"
+              v-model="user.username"
+            ></v-textarea>
+            <v-card-text>
+              New password:
+            </v-card-text>
+            <v-textarea
+              outline
+              label="Password"
+              v-model="user.password"
+            ></v-textarea>
+            <v-card-text>
+              Select roles:
+            </v-card-text>
+            <v-checkbox
+              label="admin"
+              v-model="adminCheckbox"
+            >
+            </v-checkbox>
+            <v-checkbox
+              label="user"
+              v-model="userCheckbox"
+            >
+            </v-checkbox>
+          </v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn color="blue darken-1" flat @click="dialog = false">Cancel</v-btn>
+            <v-btn color="blue darken-1" flat @click="updateUser">Edit</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
       <v-data-table
         :headers="headers"
         :items="users"
@@ -26,6 +69,12 @@
             <td>{{ props.item.username }}</td>
             <td>{{ props.item.createdAt }}</td>
             <td>
+              <v-icon
+                small
+                @click="editUser(props.item.id)"
+              >
+                edit
+              </v-icon>
               <v-icon
                 small
                 @click="deleteUser(props.item.id)"
@@ -48,6 +97,7 @@ import api from '../../api'
 export default {
   data () {
     return {
+      dialog: false,
       search: '',
       pagination: {},
       selected: [],
@@ -58,9 +108,17 @@ export default {
         { text: 'Actions', value: 'id' }
       ],
       users: [],
+      user: {
+        id: -1,
+        username: '',
+        password: '',
+        roles: []
+      },
       time: 1000,
       alert: false,
-      errorMessage: ''
+      errorMessage: '',
+      userCheckbox: false,
+      adminCheckbox: false
     }
   },
 
@@ -77,11 +135,62 @@ export default {
   mounted () {
     this.checkUsers()
   },
+
   methods: {
     createUser: function () {
       this.$router.push('/create')
       this.checkUsers()
     },
+
+    editUser: function (id) {
+      this.dialog = true
+      this.user.id = id
+    },
+
+    updateUser: function () {
+      if (this.adminCheckbox) {
+        this.user.roles.push('admin')
+      }
+      if (this.userCheckbox) {
+        this.user.roles.push('user')
+      }
+      var updatedUser = {
+        id: this.user.id
+      }
+      if (this.user.username !== '') {
+        updatedUser['username'] = this.user.username
+      }
+      if (this.user.password !== '') {
+        updatedUser['password'] = this.user.password
+      }
+      if (this.user.roles.length > 0) {
+        updatedUser['roles'] = this.user.roles
+      }
+      api
+        .request('put', '/user', this.$store.state.accessToken, updatedUser)
+        .then(response => {
+          this.user = {
+            id: -1,
+            username: '',
+            password: '',
+            roles: []
+          }
+          this.adminCheckbox = false
+          this.userCheckbox = false
+          this.dialog = false
+          this.checkUsers()
+        })
+        .catch(error => {
+          this.pagination = {}
+          if (!error.hasOwnProperty('response')) {
+            this.errorMessage = error.message
+          } else {
+            this.errorMessage = error.response.data.msg
+          }
+          this.alert = true
+        })
+    },
+
     checkUsers: function () {
       api
         .request('get', '/users', this.$store.state.accessToken)
@@ -92,10 +201,15 @@ export default {
         })
         .catch(error => {
           this.pagination = {}
-          this.errorMessage = error.response.data.msg
+          if (!error.hasOwnProperty('response')) {
+            this.errorMessage = error.message
+          } else {
+            this.errorMessage = error.response.data.msg
+          }
           this.alert = true
         })
     },
+
     deleteUser: function (userId) {
       api
         .request('delete', '/user/delete/' + userId, this.$store.state.accessToken)
@@ -103,7 +217,11 @@ export default {
           this.checkUsers()
         })
         .catch(error => {
-          this.errorMessage = error.response.data.msg
+          if (!error.hasOwnProperty('response')) {
+            this.errorMessage = error.message
+          } else {
+            this.errorMessage = error.response.data.msg
+          }
           this.alert = true
         })
     }
