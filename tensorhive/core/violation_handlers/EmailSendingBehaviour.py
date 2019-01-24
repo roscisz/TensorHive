13b246f0e520f641864d1104e8e4dc21723e5be9
Michał Martyniak <michal.martyniak@linux.pl>
2019-01-24 22:03:23 +0100

    Fix incorrect 'if' statement for email time counter

--- tensorhive/core/violation_handlers/EmailSendingBehaviour.py
+++ tensorhive/core/violation_handlers/EmailSendingBehaviour.py
@@ -51,17 +51,20 @@ class EmailSendingBehaviour:
             # Check if time is out
             timenow = datetime.datetime.utcnow()
             if timer:
-                if timenow <= timer + self.interval:
+                if timer + self.interval <= timenow:
                     # We should penalize that session
                     result.append(session)
                     del time_left[username]
                 else:
+                    #FIXME Remove this later
+                    log.debug('Delaying email Next attempt in {}'.format(self.interval - (timenow - timer)))
                     # That session is not ready to be penalized
                     pass
             else:
                 # We should penalize any new unauthorized session
                 time_left[username] = timenow
                 result.append(session)
+        # self.time_between_notifications = time_left
         return result
 
     @override
