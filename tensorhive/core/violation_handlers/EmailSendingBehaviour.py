c1e4985e0974fd6234588792efd6478a87a201de
Michał Martyniak <michal.martyniak@linux.pl>
2019-01-25 16:59:59 +0100

    Refactor, much cleaner code

--- tensorhive/core/violation_handlers/EmailSendingBehaviour.py
+++ tensorhive/core/violation_handlers/EmailSendingBehaviour.py
@@ -1,7 +1,7 @@
 from tensorhive.core.utils.decorators.override import override
 from sqlalchemy.orm.exc import NoResultFound
 from tensorhive.models.User import User
-from typing import Generator, Dict, List, Any
+from typing import Generator, Dict, List, Any, Optional
 from tensorhive.config import MAILBOT
 from tensorhive.core.utils.mailer import Mailer, Message
 from email.mime.multipart import MIMEMultipart
@@ -22,41 +22,34 @@ class EmailSendingBehaviour:
 
     def __init__(self):
         self.interval = datetime.timedelta(minutes=1)
-        self.time_between_notifications = {}
+        self.time_of_last_email = {}
         self.mailer = Mailer(server=MAILBOT.SMTP_SERVER, port=MAILBOT.SMTP_PORT)
 
-    def filter_sessions(self, sessions):
+    def filter_sessions(self, violation_data):
         result = []
-        time_left = self.time_between_notifications
-
-        for session in sessions:
-            # Fetch how much time left for specific user
-            username = session['USER']
-            timer = time_left.get(username)
-
-            # Check if time is out
-            timenow = datetime.datetime.utcnow()
-            if timer:
-                if timer + self.interval <= timenow:
-                    # We should penalize that session
-                    result.append(session)
-                    del time_left[username]
-                else:
-                    #FIXME Remove this later
-                    log.debug('Email warning postponed, next attempt in {}'.format(self.interval - (timenow - timer)))
-                    # That session is not ready to be penalized
-                    pass
-            else:
-                # We should penalize any new unauthorized session
-                time_left[username] = timenow
-                result.append(session)
         # self.time_between_notifications = time_left
         return result
 
-    def prepare_message(self, session: Dict[str, Any], recipients: List[str]) -> Message:
+    # def time_since_last_email(self, username: str) -> datetime.timedelta:
+    #     '''
+    #     Keeps track of every email time since last email was sent to
+    #     a specific user.
+    #     '''
+    #     # Use shorter name (alias)
+    #     time_left = self.time_between_notifications
+    #     last_email_time = time_left.get(username)
+    #     timenow = datetime.datetime.utcnow()
+
+    #     if last_email_time:
+    #         if last_email_time + self.interval <= timenow:
+    #             del time_left[username]
+    #     else:
+    #         time_left[username] = timenow
+
+    def prepare_message(self, violation_data: Dict[str, Any], recipients: List[str]) -> Message:
         email_body = self.message.format(
-            legitimate_owner_username=session['LEGITIMATE_USER'],
-            gpu_uuid=session['GPU_UUID']
+            legitimate_owner_username=violation_data['RESERVATION_OWNER'],
+            gpu_uuid=violation_data['UUID']
         )
         return Message(
             author=os.getenv(MAILBOT.SMTP_LOGIN_ENV),
@@ -65,47 +58,66 @@ class EmailSendingBehaviour:
             body=email_body
         )
 
+    def fetch_email_address(self, username: str) -> Optional[str]:
+        email = None
+        try:
+            intruder = User.find_by_username(username)
+        except NoResultFound as e:
+            log.error('Cannot find email address for {}, reason: no account!'.format(username))
+        except Exception as e:
+            log.critical(e)
+        else:
+            email = intruder.email
+        finally:
+            return email
+
+    def ready_to_send(self, email_address: str) -> bool:
+        '''
+        Allows for sending email only once every given time
+        to a particular email address
+        '''
+        try:
+            ready_status = self.time_of_last_email[email_address] + self.interval < datetime.datetime.utcnow()
+        except KeyError as e:
+            ready_status = True
+        finally:
+            return ready_status
+
     @override
-    def trigger_action(self, connection, unauthorized_sessions):
-        sessions = self.filter_sessions(unauthorized_sessions)
+    def trigger_action(self, violation_data: Dict[str, Any]):
+        # TODO FRIDAY: Admin and intruder get different messages
+        try:
+            # Expect proper keys beforehand
+            assert set(['INTRUDER', 'RESERVATION_OWNER', 'UUID', 'HOSTNAME']).issubset(violation_data), \
+                'Invalid keys in violation_data'
 
-        # There's no need to go further if we have no sessions
-        # to penalize (they must wait until timer resets)
-        if not sessions:
-            return
-
-        self.mailer.connect(
-            login=os.getenv(MAILBOT.SMTP_LOGIN_ENV),
-            password=os.getenv(MAILBOT.SMTP_PASSWORD_ENV)
-        )
-
-        for session in sessions:
+            # Set email's recipients
             recipients = []
+            intruder_username = violation_data['INTRUDER']
+            intruder_email = self.fetch_email_address(username=intruder_username)
 
-            # Try to fetch intruder's account
-            try:
-                username = session['USER']
-                user = User.find_by_username(username)
-                recipients.append(user.email)
-            except NoResultFound as e:
-                log.error('Cannot send email to {}, reason: no account!'.format(username))
-            except Exception as e:
-                log.critical(e)
+            if intruder_email and self.ready_to_send(intruder_email):
+                recipients.append(intruder_email)
+            if MAILBOT.NOTIFY_ADMIN and self.ready_to_send(MAILBOT.ADMIN_EMAIL):
+                recipients.append(MAILBOT.ADMIN_EMAIL)
+            assert recipients
 
-            # Admin should always get a notification
-            # Even when intruder does not have an account
-            # That's why it's separate try/except
-            try:
-                if MAILBOT.NOTIFY_ADMIN:
-                    recipients.append(MAILBOT.ADMIN_EMAIL)
+            # Initialize mailer connection
+            self.mailer.connect(
+                login=os.getenv(MAILBOT.SMTP_LOGIN_ENV),
+                password=os.getenv(MAILBOT.SMTP_PASSWORD_ENV)
+            )
 
-                # Prepare email and send
-                assert recipients
-                email = self.prepare_message(session, recipients)
-                self.mailer.send(email)
-                log.debug('Email sent to: {}'.format(email.recipients))
-            except AssertionError as e:
-                pass
-            except Exception as e:
-                log.critical(e)
-        self.mailer.disconnect()
+            # Prepare email and send
+            email = self.prepare_message(violation_data, recipients)
+            self.mailer.send(email)
+        except AssertionError as e:
+            log.error(e)
+        except Exception as e:
+            log.critical(e)
+        else:
+            log.debug('Email sent to: {}'.format(email.recipients))
+            # Remember time email was sent
+            for email_address in recipients:
+                self.time_of_last_email[email_address] = datetime.datetime.utcnow()
+            self.mailer.disconnect()
