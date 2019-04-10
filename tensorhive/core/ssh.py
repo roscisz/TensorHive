6b8923aa1d61676bfced88d3aba7adb085c0ee25
Michal Martyniak <michal.martyniak@linux.pl>
2019-04-10 15:34:15 +0200

    Add memoize function and test

--- tensorhive/core/ssh.py
+++ tensorhive/core/ssh.py
@@ -2,7 +2,7 @@ from typing import Optional, Dict
 from pssh.clients.native import ParallelSSHClient
 import pssh
 from tensorhive.core.utils.decorators import hashable_cache, timeit
-from functools import lru_cache
+import functools
 
 __author__ = '@micmarty'
 __all__ = [
@@ -17,6 +17,8 @@ Hostname = str
 Username = str
 CommandResult = Dict[Hostname, pssh.output.HostOutput]
 
+
+
 def build_dedicated_config_for(host: Hostname, user: Username) -> HostsConfig:
     return {
         host: {
@@ -25,7 +27,35 @@ def build_dedicated_config_for(host: Hostname, user: Username) -> HostsConfig:
         }
     }
 
-@hashable_cache(lru_cache())
+def memoize(func):
+    '''Decorator which enables caching function's return values when called with the exact same arguments.
+
+    When decorated function is called with arguments that are already in cache (key exists) it won't be executed
+    but cached value will be returned instead. All arguments are serlialized into a single string.
+
+    Cached values can be inspected by: print(decorated_func.cache)
+
+    Note:
+    This is extremely rare and probably won't be exploited by accident :)
+    1) key = basic_key
+        foo(True) and foo('True') are cached under same key name
+    2) key = bulletproof_key
+        foo(True) and foo('True') will be treated separately (different key in dict)
+    '''
+    cache = func.cache = {}
+    @functools.wraps(func)
+    def memoized_func(*args, **kwargs):
+        basic_key = str(args) + str(kwargs)
+        cls_name = lambda val: val.__class__.__name__
+        bulletproof_key = basic_key + str([cls_name(arg) for arg in args]) + str([cls_name(val) for val in kwargs.values()])
+        key = bulletproof_key
+        print(key)
+        if key not in cache:
+            cache[key] = func(*args, **kwargs)
+        return cache[key]
+    return memoized_func
+
+@memoize
 def get_client(config: HostsConfig, pconfig: Optional[ProxyConfig] = None, **kwargs) -> ParallelSSHClient:
     """Builds and returns an ssh client object for given configuration.
     
