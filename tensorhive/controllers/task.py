7ed2c5e6c562b61cf4027106be40cff9c86f951c
Michal Martyniak <michal.martyniak@linux.pl>
2019-05-04 20:08:39 +0200

    Adjust controller functions, decorate with @synchronize_task_record

--- tensorhive/controllers/task.py
+++ tensorhive/controllers/task.py
@@ -151,48 +151,80 @@ def running_sessions(user_id, hostname):
         content, status = {'msg': T['running']['success'], 'pids': pids}, 200
     finally:
         return content, status
-        
+
+
+# GET /tasks/running?user_id=X&hostname=X
+# FIXME Handle exceptions etc.
+def running_tasks(user_id, hostname):
+    """List of Tasks (from database) that are actually running (active screen session)"""
+    assert user_id and hostname
+    user = User.get(user_id)
+    pids = task_nursery.running(host=hostname, user=user.username)
+    tasks = Task.query.filter(Task.pid.in_(pids)).all()
+    return {'msg': T['running']['success'], 'tasks': [task.as_dict for task in tasks]}, 200
+
 
 # GET /tasks/{id}/terminate
+@synchronize_task_record
 def terminate(id):
-    # FIXME Check for pid being None
     try:
         task = Task.get(id)
-        assert task.pid and task.exit_code is None
-        exit_code = task_nursery.terminate(task.pid, task.host, task.user.username)
+        assert task.status is TaskStatus.running, 'Only running tasks can be terminated'
+        assert task.pid, 'Task has no pid assigned'  # It means there's inconsistency
+        exit_code = task_nursery.terminate(task.pid, task.host, task.user.username, gracefully=True)
 
-        # Allow to spawn that object again
+        # Allow to spawn that task again
         task.pid = None
-        task.exit_code = exit_code
+        task.status = TaskStatus.terminated
         task.save()
-    except AssertionError:
-        content, status = {'msg': T['terminate']['failure']['invalid_state']}, 405
-    except AssertionError:
-        raise NotImplementedError
+    except AssertionError as e:
+        # FIXME
+        content, status = {'msg': str(e)}, 405
+    # TODO What if terminate could not connect?
     else:
         # FIXME Display success only with code==0
-        content, status = {'msg': T['terminate']['success'], 'exit_code': exit_code}, 200
+        if exit_code == 0:
+            content, status = {'msg': T['terminate']['success'], 'exit_code': exit_code}, 123
+        else:
+            content, status = {'msg': 'Termination operation failed', 'exit_code': exit_code}, 200
     finally:
         return content, status
 
-# GET /tasks/{id}/start
+
+# GET /tasks/{id}/spawn
 #@jwt_required
+@synchronize_task_record
 def spawn(id):
     # FIXME Check exceptions, etc.
     # FIXME Redesign
     try:
         task = Task.get(id)
-        assert task.pid is None
+        assert task.status is not TaskStatus.running, 'Task juz jest zespawnowany (może musisz wywołać sync)'
+        assert task.command, 'command is empty'
+        assert task.host, 'hostname is empty'
+        assert task.user, 'user does not exist'
+        print(task.as_dict)
         pid = task_nursery.spawn(task.command, task.host, task.user.username)
         task.pid = pid
-        task.exit_code = None
+        task.status = TaskStatus.running
         task.save()
-    except AssertionError:
-        content, status = {'msg': T['spawn']['failure']['already_spawned']}, 405    
-    except AssertionError:
-        raise NotImplementedError
+    # except AssertionError as e:
+    #     print(e)
+    #     content, status = {
+    #         'msg': T['spawn']['failure']['already_spawned']
+    #     }, 405
+    except NoResultFound:
+        # FIXME
+        content, status = {'msg': 'Task with id={} does not exist'.format(id)}, 123
+    except AssertionError as e:
+        # FIXME
+        content, status = {'msg': 'Unable to spawn task, reason: {}'.format(e)}, 123
+    except Exception as e:
+        # FIXME
+        content, status = str(e), 500
     else:
         content, status = {'msg': T['spawn']['success'], 'pid': pid}, 200
     finally:
+        print('=================')
         return content, status
 
