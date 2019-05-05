d97d4cf9de9a2952bf014ac0ab89b50cc03e583b
Michal Martyniak <michal.martyniak@linux.pl>
2019-05-05 16:56:03 +0200

    Update controller functions' descriptions. Fix use of invalid `123` status code

--- tensorhive/controllers/task.py
+++ tensorhive/controllers/task.py
@@ -20,17 +20,28 @@ G = API.RESPONSES['general']
 
 
 def synchronize(task_id: int) -> None:
-    """Updates state of a Task stored in database.
+    """Updates state of a Task object stored in database.
 
     It compares current db record with list of active screen session (their pids in general)
-    on node defined by Task object (task.user.username@task.host)
+    on node defined by that record (task.user.username@task.host).
+
+    If task_nursery is unable to fetch active screen sessions then
+    the new state is always set to unsynchronized.
+
+    If task.pid is not alive (db record is outdated), then it
+    makes transition from last known state to a new state:
+
+    state before sync   => state applied after sync
+    -----------------------------------------------
+    running             => terminated
+    unsynchronized      => not_running
     """
     print('Syncing Task {}...'.format(task_id))
     try:
         task = Task.get(task_id)
         assert task.host, 'hostname is empty'
         assert task.user, 'user does not exist'
-        pids = task_nursery.running(host=task.host, user=task.user.username)
+        active_sessions_pids = task_nursery.running(host=task.host, user=task.user.username)
     except NoResultFound:
         # This exception must be handled within try/except block when using Task.get()
         pass
@@ -41,10 +52,7 @@ def synchronize(task_id: int) -> None:
         task.save()
         print('Task {} current status is: {}'.format(task_id, task.status))
     else:
-        if task.pid in pids:
-            # Nothing to do
-            pass
-        else:
+        if task.pid not in active_sessions_pids:
             if task.status is TaskStatus.running:
                 task.status = TaskStatus.terminated
             if task.status is TaskStatus.unsynchronized:
@@ -56,7 +64,9 @@ def synchronize(task_id: int) -> None:
 
 def synchronize_task_record(func: Callable[[int], Any]) -> Callable[[int], Any]:
     """Decorated function MUST CONTAIN task id (int), function can take more arguments though.
-    If task id could not be obtained from wrapped function's arguments, synchronization will be aborted.
+
+    In case when task.id could not be obtained from wrapped function's arguments,
+    synchronization will be aborted, but it won't affect wrapped function (silent fail).
     """
 
     @wraps(func)
@@ -101,10 +111,16 @@ def create(task):
         new_task = Task(user_id=task['userId'], host=task['hostname'], command=task['command'])
         assert all(task.values()), 'fields cannot be blank or null'
         new_task.save()
+    except KeyError as e:
+        # FIXME
+        content = {'msg': 'TODO Bad request' + str(e)}
+        status = 422
     except AssertionError as e:
+        # FIXME
         content = {'msg': T['create']['failure']['invalid'].format(reason=e)}
         status = 422
     except Exception as e:
+        # FIXME
         print(e)
         content = {'msg': G['internal_error']}
         status = 500
@@ -138,7 +154,7 @@ def get(id):
         return content, status
 
 
-# PUT /tasks/{id}:
+# PUT /tasks/{id}
 def update(id: int, new_values: Dict[str, Any]):
     allowed_fields = {'command', 'hostname'}
     try:
@@ -147,18 +163,18 @@ def update(id: int, new_values: Dict[str, Any]):
 
         for field_name, new_value in new_values.items():
             if field_name == 'hostname':
-                # API client is allowed to use more verbose name here
+                # API client is allowed to use more verbose name here (hostname <=> host)
                 field_name = 'host'
             else:
-                # Check every other field matches
+                # Check that every other field matches
                 assert hasattr(task, field_name), 'task object has no {} attribute'.format(field_name)
             setattr(task, field_name, new_value)
         task.save()
     except NoResultFound:
         content = {'msg': 'TODO Not found'}
-        status = 123
-    # except KeyError as e:
+        status = 404
     # FIXME Remove, won't occur
+    # except KeyError as e:
     #     content = {'msg': 'TODO Mismatche' + str(e)}
     #     status = 400
     except AssertionError as e:
@@ -181,7 +197,7 @@ def destroy(id: int):
     except NoResultFound:
         # FIXME
         content = {'msg': 'TODO Not found'}
-        status = 123
+        status = 404
     except Exception as e:
         # FIXME
         content, status = {'msg': G['internal_error'] + str(e)}, 500
@@ -196,8 +212,8 @@ def destroy(id: int):
 def screen_sessions(username: str, hostname: str) -> List[int]:
     """Returns pids of running `screen` sessions.
 
-    This endpoint is meant for development purposes,
-    there's no need to use it
+    This endpoint is for purely development purposes,
+    currently there's no need to use it.
     """
     try:
         assert username and hostname, 'arguments must not be empty'
@@ -215,41 +231,45 @@ def screen_sessions(username: str, hostname: str) -> List[int]:
         return content, status
 
 
-# GET /tasks/running?user_id=X&hostname=X
-# FIXME Remove, unused - replaced by synchronization method
-# FIXME Handle exceptions etc.
-# def running_tasks(user_id, hostname):
-#     """List of Tasks (from database) that are actually running (active screen session)"""
-#     assert user_id and hostname
-#     user = User.get(user_id)
-#     pids = task_nursery.running(host=hostname, user=user.username)
-#     tasks = Task.query.filter(Task.pid.in_(pids)).all()
-#     return {'msg': T['running']['success'], 'tasks': [task.as_dict for task in tasks]}, 200
-
-
 # GET /tasks/{id}/terminate
 @synchronize_task_record
 def terminate(id):
+    """Sends SIGINT (default) or SIGKILL to process with pid that is stored in Task db record.
+
+    In order to send SIGKILL, pass `gracefully=False` to `terminate` function.
+    Note that:
+    1) `exit_code` is related to executing kill operation, not killed process.
+    2) termination signal should be respected by most processes, however this
+        function does not guarantee stoping the process!
+    """
     try:
         task = Task.get(id)
         assert task.status is TaskStatus.running, 'Only running tasks can be terminated'
         assert task.pid, 'Task has no pid assigned'  # It means there's inconsistency
+
         exit_code = task_nursery.terminate(task.pid, task.host, task.user.username, gracefully=True)
 
         # Allow to spawn that task again
         task.pid = None
         task.status = TaskStatus.terminated
         task.save()
+    except NoResultFound:
+        # FIXME
+        content, status = {'msg': 'TODO Not found'}, 404
     except AssertionError as e:
         # FIXME
         content, status = {'msg': str(e)}, 405
     # TODO What if terminate could not connect?
+    except Exception as e:
+        # FIXME
+        content, status = {'msg': 'TODO Exception' + str(e)}, 500
     else:
-        # FIXME Display success only with code==0
         if exit_code == 0:
-            content, status = {'msg': T['terminate']['success'], 'exit_code': exit_code}, 123
+            # FIXME
+            content, status = {'msg': T['terminate']['success'], 'exit_code': exit_code}, 200
         else:
-            content, status = {'msg': 'Termination operation failed', 'exit_code': exit_code}, 200
+            # FIXME
+            content, status = {'msg': 'Termination operation failed', 'exit_code': exit_code}, 400
     finally:
         return content, status
 
@@ -258,15 +278,17 @@ def terminate(id):
 #@jwt_required
 @synchronize_task_record
 def spawn(id):
-    # FIXME Check exceptions, etc.
-    # FIXME Redesign
+    """Spawns command stored in Task db record (task.command).
+
+    It won't allow for spawning task which is currently running (sync + status check).
+    If spawn operation has succeeded then `running` status is set.
+    """
     try:
         task = Task.get(id)
         assert task.status is not TaskStatus.running, 'Task juz jest zespawnowany (może musisz wywołać sync)'
         assert task.command, 'command is empty'
         assert task.host, 'hostname is empty'
         assert task.user, 'user does not exist'
-        print(task.as_dict)
         pid = task_nursery.spawn(task.command, task.host, task.user.username)
         task.pid = pid
         task.status = TaskStatus.running
@@ -278,10 +300,10 @@ def spawn(id):
     #     }, 405
     except NoResultFound:
         # FIXME
-        content, status = {'msg': 'Task with id={} does not exist'.format(id)}, 123
+        content, status = {'msg': 'Task with id={} does not exist'.format(id)}, 404
     except (AssertionError, SpawnError) as e:
         # FIXME
-        content, status = {'msg': 'Unable to spawn task, reason: {}'.format(e)}, 123
+        content, status = {'msg': 'Unable to spawn task, reason: {}'.format(e)}, 422
     except Exception as e:
         # FIXME
         content, status = str(e), 500
@@ -295,28 +317,40 @@ def spawn(id):
 if __name__ == '__main__':
     import os
     from tensorhive.database import init_db
+    from inspect import cleandoc
     init_db()
 
     user = '155136mm'
     host = 'galileo.eti.pg.gda.pl'
-    cmd = './long.sh'  # cd ~/Simulators/095; DISPLAY= ./CarlaUE4.sh Town04'
+    cmd = './long.sh'
+    """
+    Example: create script named long.sh
+    ```
+    for i in {1..60}
+    do
+        echo "Counting: $i"
+        sleep 1s
+    done
+    ```
+    """
 
     wait_for_clear = lambda: input('Press enter to show menu...')
     while True:
-        print('''
-==> Manual task controller test suite: <==
+        print(
+            cleandoc('''
+            ==> Manual task controller test suite: <==
 
-1) Create task record
-2) Spawn (id)
-3) Get one (id)
-4) Get multiple (all or by user id)
-5) Terminate (id)
-6) Update task command and hostname (id)
-7) Destroy task (id)
-8) Create random user with 3 tasks
-9) Destroy user (user id) - cascade deletion test
-Any other key to clear console
-        ''')
+            1) Create task record
+            2) Spawn (id)
+            3) Get one (id)
+            4) Get multiple (all or by user id)
+            5) Terminate (id)
+            6) Update task command and hostname (id)
+            7) Destroy task (id)
+            8) Create random user with 3 tasks
+            9) Destroy user (user id) - cascade deletion test
+            Any other key to clear console
+        '''))
         action = input('> ')[0]
         if action == '1':
             content, status = create(dict(userId=1, hostname=host, command=cmd))
