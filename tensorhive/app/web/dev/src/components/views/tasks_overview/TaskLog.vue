41d5dc60d9fc4d0ab68c9598e9a05c216dd9c914
Jacek Szemplinski <j.szemplinski@wp.eu>
2020-03-13 14:04:52 +0100

    Add tail mode and refresh feature to the log preview

--- tensorhive/app/web/dev/src/components/views/tasks_overview/TaskLog.vue
+++ tensorhive/app/web/dev/src/components/views/tasks_overview/TaskLog.vue
@@ -14,7 +14,36 @@
         >
           <v-icon>close</v-icon>
         </v-btn>
-        <span class="headline">Task log</span>
+        <span class="headline">
+          Task log
+          <v-btn
+            flat
+            icon
+            color="green"
+            @click="refresh()">
+            <v-icon>refresh</v-icon>
+          </v-btn>
+        </span>
+        <span class="subheading">
+          <v-checkbox
+            flat
+            style="display: inline"
+            label="Tail mode"
+            v-model="tailMode"
+            @click="refresh"
+            hide-details
+           />
+          <v-checkbox
+            flat
+            style="display: inline"
+            label="Auto-refresh"
+            v-model="autoRefresh"
+            v-on="on"
+            :disabled="!tailMode"
+            @click="toggleAutoRefresh"
+            hide-details
+          />
+        </span>
       </v-card-text>
       <v-card-text>
         {{path}}
@@ -33,11 +62,15 @@ export default {
   props: {
     showModal: Boolean,
     lines: Array,
-    path: String
+    path: String,
+    taskId: Number
   },
   data () {
     return {
-      show: false
+      show: false,
+      tailMode: false,
+      autoRefresh: false,
+      autoRefreshIntervalId: -1
     }
   },
   watch: {
@@ -51,6 +84,21 @@ export default {
   methods: {
     close: function () {
       this.$emit('close')
+    },
+    refresh: function () {
+      this.$emit('getLog', this.taskId, this.tailMode)
+      if (this.autoRefresh && !this.tailMode) {
+        this.autoRefresh = false
+        this.toggleAutoRefresh()
+      }
+    },
+    toggleAutoRefresh: function () {
+      if (this.autoRefresh) {
+        this.autoRefreshIntervalId = window.setInterval(this.refresh, 5000)
+      } else {
+        window.clearInterval(this.autoRefreshIntervalId)
+        this.autoRefreshIntervalId = -1
+      }
     }
   }
 }
