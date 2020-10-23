358f3ac92d681257a6b7c0c71bf3c65d56c70a59
Jacek Szemplinski <j.szemplinski@wp.eu>
2020-10-23 14:31:07 +0200

    Add ability to mark groups as default - DB part

--- tensorhive/controllers/user.py
+++ tensorhive/controllers/user.py
@@ -13,6 +13,7 @@ from tensorhive.core.managers.TensorHiveManager import TensorHiveManager
 from tensorhive.models.RevokedToken import RevokedToken
 from tensorhive.models.Role import Role
 from tensorhive.models.User import User
+from tensorhive.models.Group import Group
 
 log = logging.getLogger(__name__)
 GENERAL = API.RESPONSES['general']
@@ -57,6 +58,10 @@ def do_create(user: Dict[str, Any]) -> Tuple[Content, HttpStatusCode]:
             roles=[Role(name='user')]
         )
         new_user.save()
+
+        default_group = Group.get_default_group()
+        if default_group is not None:
+            default_group.add_user(new_user)
     except AssertionError as e:
         content = {'msg': USER['create']['failure']['invalid'].format(reason=e)}
         status = 422
--- tensorhive/models/Group.py
+++ tensorhive/models/Group.py
@@ -1,8 +1,9 @@
 import datetime
 import logging
 
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.ext.hybrid import hybrid_property
 from tensorhive.database import Base
 from tensorhive.exceptions.InvalidRequestException import InvalidRequestException
@@ -21,6 +22,7 @@ class Group(CRUDModel, RestrictionAssignee):  # type: ignore
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String(40), unique=False, nullable=True)
     created_at = Column(DateTime, default=datetime.datetime.utcnow)
+    _is_default = Column('is_default', Boolean, nullable=True, unique=True)  # Should be True or None, never False
 
     _users = relationship('User', secondary='user2group')
     _restrictions = relationship('Restriction', secondary='restriction2assignee')
@@ -29,7 +31,8 @@ class Group(CRUDModel, RestrictionAssignee):  # type: ignore
         return '<Group id={id}, name={name}>'.format(id=self.id, name=self.name)
 
     def check_assertions(self):
-        pass
+        assert self._is_default is None or self._is_default is True, 'is_default should be either set to True for' \
+                                                                     'the default group or None for the rest'
 
     @hybrid_property
     def users(self):
@@ -75,6 +78,44 @@ class Group(CRUDModel, RestrictionAssignee):  # type: ignore
         if include_users:
             group['users'] = [user.as_dict_shallow for user in self.users]
         return group
+
+    @classmethod
+    def get_default_group(cls):
+        """
+        :raises: MultipleResultsFound if more than one default group is found
+        :return: A group that is marked as default or None if no such group exists
+        """
+        return Group.query.filter(Group.is_default.is_(True)).one_or_none()
+
+    @classmethod
+    def set_default_group(cls, group_id):
+        """
+        Sets the group with id = group_id as a default group.
+        Will also unmark the existing default group as the default.
+        :raises: NoResultFound if group with given id doesn't exist.
+        :return: The default group.
+        """
+        try:
+            group = Group.get(group_id)
+        except NoResultFound as e:
+            raise e
+
+        cls.delete_default_group_if_exists()
+        group.is_default = True
+        return group.save()
+
+    @classmethod
+    def delete_default_group_if_exists(cls):
+        """
+        Will mark the default group as non-default. If no such group exists, no action will be taken.
+        :return: True if group was marked as non-default, False if no default group was found.
+        """
+        group = cls.get_default_group()
+        if group is None:
+            return False
+        group.is_default = None
+        group.save()
+        return True
 
 
 class User2Group(Base):  # type: ignore
