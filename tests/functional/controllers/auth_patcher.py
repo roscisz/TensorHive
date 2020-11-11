ce63b6bc7b2c9b88111df4d7e0ffa46bad62b019
Paweł Rościszewski <pawel.rosciszewski@pg.edu.pl>
2020-11-11 17:31:10 +0100

    add auth_patcher to functional tests
    divide group controller tests to normal and superuser

---
+++ tests/functional/controllers/auth_patcher.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+from tensorhive.config import API
+from functools import wraps
+
+G = API.RESPONSES['general']
+
+
+def get_patch(superuser=False):
+    def always_unprivileged(fn):
+        @wraps(fn)
+        def wrapper(*args, **kwargs):
+            return {'msg': G['unprivileged']}, 403
+
+        return wrapper
+
+    if superuser:
+        return patch('tensorhive.authorization.admin_required', lambda x: x)
+    else:
+        return patch('tensorhive.authorization.admin_required', lambda x: always_unprivileged(x))
