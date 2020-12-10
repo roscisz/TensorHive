831ec5a691015c935a945ee40e7c133c12fc34f3
Szymon Gebler <gebsh@gebshMac.local>
2020-12-10 22:28:41 +0100

    feat: add tasks table

---
+++ tensorhive/app/web/dev/src/components/views/tasks_overview/TaskBulkActions.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-layout>
+    <v-select
+      v-model="action"
+      solo
+      hide-details
+      color="primary"
+      label="Action"
+      :items="actions"
+    ></v-select>
+    <v-btn
+      color="primary"
+      :loading="performingAction"
+      :small="smallPerformButton"
+      @click="$emit('action', action)"
+    >
+      Perform
+    </v-btn>
+  </v-layout>
+</template>
+
+<script>
+export const TaskBulkActions = {
+  Execute: 'Execute',
+  Stop: 'Stop',
+  Kill: 'Kill',
+  Copy: 'Copy',
+  Delete: 'Delete'
+}
+
+export default {
+  props: {
+    performingAction: {
+      type: Boolean,
+      default: false
+    },
+    smallPerformButton: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data () {
+    return {
+      action: undefined,
+      actions: Object.values(TaskBulkActions)
+    }
+  }
+}
+</script>
